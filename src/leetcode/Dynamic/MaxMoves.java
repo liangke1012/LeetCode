2e2ef38bbbc4f2a881bdb43ad32f83d4a7b5f39e
liangke <liangke15@huawei.com>
2024-03-16 17:24:06 +0800

    0316每日一题

---
+++ src/leetcode/Dynamic/MaxMoves.java
@@ -0,0 +1,12 @@
+package leetcode.Dynamic;
+
+public class MaxMoves {
+    public static void main(String[] args) {
+        int[][] grid = {{2,4,3,5},{5,4,9,3},{3,4,2,11},{10,9,13,15}};
+        int res = maxMoves(grid);
+    }
+
+    private static int maxMoves(int[][] grid) {
+
+    }
+}
---
+++ src/leetcode/Dynamic/SellingWood_2312.java
@@ -0,0 +1,15 @@
+package leetcode.Dynamic;
+
+public class SellingWood_2312 {
+    public static void main(String[] args) {
+        int m = 4;
+        int n = 6;
+        int[][] prices = {{3,2,10},{1,4,2},{4,1,3}};
+        long res = sellingWood(m, n, prices);
+        System.out.println(res);
+    }
+
+    private static long sellingWood(int m, int n, int[][] prices) {
+
+    }
+}
