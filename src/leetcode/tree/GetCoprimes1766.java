6b9a358a0cc10b837c3444102f8c95a81a41f1ed
liangke <liangke15@huawei.com>
2024-04-11 19:17:03 +0800

    04/11 每日一题（不会）

---
+++ src/leetcode/tree/GetCoprimes1766.java
@@ -0,0 +1,14 @@
+package leetcode.tree;
+
+public class GetCoprimes1766 {
+    public static void main(String[] args) {
+        int[] nums = {2,3,3,2};
+        int[][] edges = new int[][]{{0,1},{1,2},{1,3}};
+        int[] res = getCoprimes(nums, edges);
+
+    }
+
+    private static int[] getCoprimes(int[] nums, int[][] edges) {
+        return new int[]{};
+    }
+}
