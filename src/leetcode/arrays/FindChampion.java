1966b94039f04e04dc33bb79b22d686c1003c926
liangke <liangke15@huawei.com>
2024-04-13 10:23:27 +0800

    04/13 每日一题

--- src/leetcode/arrays/FindChampion.java
+++ src/leetcode/arrays/FindChampion.java
@@ -21,4 +21,22 @@ public class FindChampion {
         }
         return -1;
     }
+
+    private static int findChampionTwo(int n, int[][] edges) {
+        int[] degree = new int[n];
+        for(int[] e : edges) {
+            degree[e[1]]++;
+        }
+        int champion = -1;
+        for (int i = 0; i < n; i++) {
+            if (degree[i] == 0) {
+                if (champion == -1){
+                    champion = i;
+                } else {
+                    return -1;
+                }
+            }
+        }
+        return champion;
+    }
 }
